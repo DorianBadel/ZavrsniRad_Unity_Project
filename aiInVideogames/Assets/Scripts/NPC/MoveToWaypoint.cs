9cd3208921e73b194f4d760b34530afc1396c3b0
Dorian Badel <DorianBadel@protonmail.com>
2022-11-21 23:59:25 +0100

    Waypoints base logic

--- aiInVideogames/Assets/Scripts/NPC/MoveToWaypoint.cs
+++ aiInVideogames/Assets/Scripts/NPC/MoveToWaypoint.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class MoveToWaypoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+  private int destinationIndex = 0;
+  private GameMaster gameMaster;
+  public int type = 1;
+
+  [Header("Required")]
+  public Transform[] waypoints;
+
+  [Header("Adjustable variables")]
+  public float moveSpeed = 10f;
+  public float stoppingDistance = 0.5f;
+  public float rotationSpeed = 3f;
+
+  void Start()
+  {
+    gameMaster = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameMaster>();
+
+    if (waypoints.Length <= 1)
     {
-        
+      Debug.Log("Game object" + this.name + " doesn't have enough waypoints assigned");
+    }
+  }
+
+
+  void Update()
+  {
+    if (gameMaster.ShouldMove(type))
+    {
+      MoveToWaypoints();
     }
 
-    // Update is called once per frame
-    void Update()
+  }
+
+  private void MoveToWaypoints()
+  {
+    if (waypoints.Length > 0)
     {
-        
+      Quaternion rotateTowards = Quaternion.LookRotation(waypoints[destinationIndex].position - transform.position);
+
+      if (type != 1)
+      {
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotateTowards, rotationSpeed * Time.deltaTime);
+      }
+
+      transform.position = Vector3.MoveTowards(transform.position, waypoints[destinationIndex].position, moveSpeed * Time.deltaTime);
+
+      //Check if has reached waypoint
+      if (Vector3.Distance(transform.position, waypoints[destinationIndex].position) < stoppingDistance)
+      {
+        destinationIndex++;
+        if (destinationIndex == waypoints.Length) destinationIndex = 0;
+      }
     }
+  }
 }
--- aiInVideogames/Assets/Scripts/Tools/GameMaster.cs
+++ aiInVideogames/Assets/Scripts/Tools/GameMaster.cs
@@ -4,15 +4,111 @@ using UnityEngine;
 
 public class GameMaster : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+  private GameObject player;
+  public bool mazeEnemiesMove = true;
+  public bool navmeshWallsMove = false;
+  public bool keyShouldHide = true;
+  private PlayerStats playerStats;
+
+  [Header("Requirements")]
+  public Camera topDownCameraMaze;
+  public Camera topDownCameraNavMesh;
+  public Transform mazeRespawnPoint;
+  private GameObject mazeKey;
+
+  void Awake()
+  {
+    player = GameObject.FindGameObjectWithTag("Player");
+    playerStats = player.GetComponent<PlayerStats>();
+
+    mazeKey = GameObject.FindGameObjectWithTag("MazeKey");
+  }
+
+  void Update()
+  {
+    if (topDownCameraNavMesh.enabled && Input.GetKeyDown(KeyCode.Escape))
     {
-        
+      FirstPersonCamera();
     }
 
-    // Update is called once per frame
-    void Update()
+    if (mazeEnemiesMove && Input.GetKeyDown(KeyCode.F))
     {
-        
+      if (topDownCameraMaze.enabled) FirstPersonCamera();
+      else TopDownMazeCamera();
     }
+  }
+
+  public void ShouldKeyHide(bool answer)
+  {
+    keyShouldHide = answer;
+  }
+
+  public void TopDownMazeCamera()
+  {
+    topDownCameraMaze.enabled = true;
+    playerStats.SetFirstPersonControlls(false);
+    keyShouldHide = true;
+    playerStats.HasKey = false;
+    mazeEnemiesMove = true;
+    Cursor.lockState = CursorLockMode.Locked;
+  }
+
+  public void TopDownNavMeshCamera()
+  {
+    topDownCameraMaze.enabled = false;
+    topDownCameraNavMesh.enabled = true;
+    playerStats.SetFirstPersonControlls(false);
+    playerStats.HasKey = false;
+    playerStats.IsDisabled = true;
+    mazeEnemiesMove = false;
+    navmeshWallsMove = true;
+    Cursor.visible = true;
+    Cursor.lockState = CursorLockMode.None;
+  }
+
+  public void FirstPersonCamera()
+  {
+    topDownCameraMaze.enabled = false;
+    topDownCameraNavMesh.enabled = false;
+    playerStats.SetFirstPersonControlls(true);
+    playerStats.IsDisabled = false; //TODO move to the script that uses it
+    Cursor.visible = false;
+    Cursor.lockState = CursorLockMode.Locked;
+
+  }
+
+  public void PlayerDetected()
+  {
+    //Preventing the player to move
+    playerStats.SetDetected(true);
+
+    //Move him to first person camera and prevent AI from moving (for effect)
+    FirstPersonCamera();
+    mazeEnemiesMove = false;
+    //TODO UI info
+    StartCoroutine("respawn");
+  }
+
+  public bool ShouldMove(int type)
+  {
+    switch (type)
+    {
+      case 1:
+        return navmeshWallsMove;
+      case 2:
+        return mazeEnemiesMove;
+    }
+    return false;
+  }
+
+  IEnumerator respawn()
+  {
+    yield return new WaitForSeconds(3f);
+    player.transform.position = mazeRespawnPoint.position;
+    yield return new WaitForSeconds(0.1f);
+    playerStats.SetDetected(false);
+    mazeKey.SendMessage("RespawnKey");
+    TopDownMazeCamera();
+    //TODO Remove hasKey, respawn the key if it was picked up
+  }
 }
