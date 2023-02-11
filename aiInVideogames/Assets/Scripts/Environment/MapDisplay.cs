5cfb370a4a8d62dd2dabfa798fa5f0b4d40cfafd
Dorian Badel <DorianBadel@protonmail.com>
2023-02-11 17:43:43 +0100

    Colliders added

--- aiInVideogames/Assets/Scripts/Environment/MapDisplay.cs
+++ aiInVideogames/Assets/Scripts/Environment/MapDisplay.cs
@@ -7,6 +7,7 @@ public class MapDisplay : MonoBehaviour
   public Renderer textureRender;
   public MeshFilter meshFilter;
   public MeshRenderer meshRenderer;
+  public MeshCollider meshCollider;
 
   public void DrawTexture(Texture2D texture)
   {
@@ -20,5 +21,6 @@ public class MapDisplay : MonoBehaviour
   {
     meshFilter.sharedMesh = meshData.CreateMesh();
     meshRenderer.sharedMaterial.mainTexture = texture;
+    meshCollider.sharedMesh = meshFilter.sharedMesh;
   }
 }
