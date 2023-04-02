de82cc2c63c240a950470d8524a4ea6021b9e109
Dorian Badel <DorianBadel@protonmail.com>
2023-04-03 00:14:40 +0200

    Boids are boiding

--- aiInVideogames/Assets/Scripts/BOIDS/Boid.cs
+++ aiInVideogames/Assets/Scripts/BOIDS/Boid.cs
@@ -8,10 +8,15 @@ public class Boid : MonoBehaviour
 
   public Boid[] allBoids;
   public List<Boid> flock;
-  // Start is called before the first frame update
+  public Vector3 calculatedDirection;
+
+  public float separationPull = 5f;
+  public float alignmentPull = 5f;
+  public float cohesionPull = 5f;
   void Start()
   {
     allBoids = FindObjectsOfType<Boid>();
+    calculatedDirection = transform.forward;
   }
 
 
@@ -23,10 +28,17 @@ public class Boid : MonoBehaviour
   // Update is called once per frame
   void Update()
   {
+    calculatedDirection.Normalize();
     DebugRays();
     if (allBoids.Length > 0) FindFlock();
     Separation();
     Alignment();
+    Cohesion();
+
+    Debug.DrawRay(transform.position, calculatedDirection, Color.magenta);
+
+    transform.position += calculatedDirection * Time.deltaTime;
+    transform.rotation = Quaternion.LookRotation(calculatedDirection);
 
   }
   //Separation
@@ -41,8 +53,9 @@ public class Boid : MonoBehaviour
     averagePosition /= flock.Count;
 
     //Move away from average position
-    Vector3 direction = transform.position - averagePosition;
+    Vector3 direction = (transform.position - averagePosition).normalized * separationPull;
     Debug.DrawRay(transform.position, direction, Color.red);
+    calculatedDirection += direction;
 
 
   }
@@ -56,11 +69,26 @@ public class Boid : MonoBehaviour
       averageHeading += fish.transform.forward;
     }
     averageHeading /= flock.Count;
-    Vector3 direction = averageHeading - transform.position;
+    Vector3 direction = (transform.forward + averageHeading).normalized * alignmentPull;
     Debug.DrawRay(transform.position, direction, Color.blue);
+    calculatedDirection += direction;
+
+
   }
 
   //Cohesion
+  private void Cohesion()
+  {
+    Vector3 averagePosition = Vector3.zero;
+    foreach (var fish in flock)
+    {
+      averagePosition += fish.transform.position;
+    }
+    averagePosition /= flock.Count;
+    Vector3 direction = (averagePosition - transform.position).normalized * cohesionPull;
+    Debug.DrawRay(transform.position, direction, Color.yellow);
+    calculatedDirection += direction;
+  }
 
 
   private void FindFlock()
